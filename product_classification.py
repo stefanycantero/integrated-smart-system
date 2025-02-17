e885f206dee6a5f24fb3dc50e29170b53e8d11bf
Stefany Cantero <stefanycanteroc@gmail.com>
2025-02-17 13:40:45 -0500

    Update product_classification module

--- product_classification.py
+++ product_classification.py
@@ -1,27 +1,59 @@
 import streamlit as st
+import pickle
+import numpy as np
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+from tensorflow.keras.models import model_from_json
 
-model = None # Cargar modelo (Pendiente)
+@st.cache_data
+def load_model():
+    with open("models/modelo_cnn.pkl", "rb") as file:
+        modelo_serializado = pickle.load(file)
+
+    model_json = modelo_serializado['modelo_json']
+    model = model_from_json(model_json)
+    model.set_weights(modelo_serializado['pesos'])
+    return model
+
+def predecir_imagen_con_umbral(imagen, model, umbral=0.88):
+
+    # Cargar y preprocesar la imagen
+    img_width, img_height = 150, 150
+    imagen = imagen.resize((img_width, img_height))
+    imagen = img_to_array(imagen) / 255.0
+    imagen = np.expand_dims(imagen, axis=0)
+
+    prediccion = model.predict(imagen)
+    max_prob = np.max(prediccion)
+
+    print("Probabilidad máxima:", max_prob)
+
+    class_indices = {'jeans': 0, 'sofa': 1, 'tshirt': 2, 'tv': 3}
+    # Si la probabilidad máxima es menor al umbral, se clasifica como desconocido
+    if max_prob < umbral:
+        return "Desconocido"
+    else:
+        indice_clase = np.argmax(prediccion)
+        class_labels = {v: k for k, v in class_indices.items()}
+        etiqueta = class_labels[indice_clase]
+        return etiqueta
 
 def display_product_classification():
-
     st.subheader("Clasificación automática de productos")
     st.write("Este módulo te permite organizar tu inventario con clasificación automática de productos.")
     st.write("AÑADIR Breve descripción del desarrllo del modelo y las métricas de evaluación obtenidas en entrenamiento y validación.")
 
-    # Realizar una nueva clasificación
     with st.expander("➡️Aquí tienes un video guía para utilizar este módulo"):
         st.video('https://www.youtube.com/watch?v=i5cHUTSnkGQ')
 
-    file = st.file_uploader("Carga aquí la imagen que deseas clasificar", type=["jpg", "jpeg", "png"])
+    files = st.file_uploader("Carga aquí las imágenes que deseas clasificar", type=["jpg", "jpeg", "png"], accept_multiple_files=True)
 
-    if file is not None:
-        # Procesar datos (Pendiente el preprocesamiento)
-        datos = file
+    if files:
+        model = load_model()
+        for file in files:
+            st.write("Imagen cargada correctamente")
+            st.image(file, caption="Imagen a clasificar", use_container_width=True)
 
-        # Realizar predicción
-        classification = model.predict(datos)
-
-        # Mostrar resultados
-        st.image(datos, caption='Imagen cargada')
-        st.write("Categoría del producto")
-        st.write(classification)
+            # Realizar la clasificación de la imagen
+            image = load_img(file)
+            etiqueta = predecir_imagen_con_umbral(image, model)
+            st.subheader(f"Categoría del producto: {etiqueta}")
